f949b1ba2807640eafbd55ccacd76115d81448c5
duykhanhtrandev <109634649+duykhanhtrandev@users.noreply.github.com>
2023-03-24 15:03:16 +0700

    event loop examples

---
+++ 01-nodejs/1-event-loop-examples.js/2-set-Timeout.js
@@ -0,0 +1,7 @@
+console.log('first');
+// started operating system process
+setTimeout(() => {
+  console.log('second');
+}, 0);
+console.log('third');
+// completed end exited operating system process
