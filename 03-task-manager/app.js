700dcdce3bea3fc360c35ea9e57c9c9a11914507
duykhanhtrandev <109634649+duykhanhtrandev@users.noreply.github.com>
2023-04-06 18:09:05 +0700

    setup controllers and routes folder

--- 03-task-manager/app.js
+++ 03-task-manager/app.js
@@ -1,1 +1,19 @@
-console.log('Task Manager App');
+const express = require('express');
+const app = express();
+const tasksRoutes = require('./routes/tasksRoutes');
+
+// middleware
+app.use(express.json());
+
+// routes
+app.get('/hello', (req, res) => {
+  res.send('Task Manager App!');
+});
+
+app.use('/api/v1/tasks', tasksRoutes);
+
+const port = 3000 || process.env.PORT;
+
+app.listen(port, (req, res) => {
+  console.log(`Server is listening on port: http://127.0.0.1:${port}`);
+});
