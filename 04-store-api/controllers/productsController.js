0a8be124b6e4bda0ab1ed684d35dc2840942235c
Duy Khánh <109634649+duykhanhtrandev@users.noreply.github.com>
2023-04-13 21:56:00 +0700

    sorting

--- 04-store-api/controllers/productsController.js
+++ 04-store-api/controllers/productsController.js
@@ -1,22 +1,35 @@
 const Product = require('../models/productModel');
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({
-    page: '2',
-  });
+  const products = await Product.find({}).sort('-name price');
   res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured } = req.query;
+  const { featured, company, name, sort } = req.query;
   const queryObject = {};
 
   if (featured) {
     queryObject.featured = featured === 'true' ? true : false;
   }
 
-  console.log(queryObject);
-  const products = await Product.find(queryObject);
+  if (company) {
+    queryObject.company = company;
+  }
+
+  if (name) {
+    queryObject.name = { $regex: name, $options: 'i' };
+  }
+
+  // console.log(queryObject);
+  let result = Product.find(queryObject);
+  if (sort) {
+    const sortList = sort.split(',').join(' ');
+    result = result.sort(sortList);
+  } else {
+    result = result.sort('createdAt');
+  }
+  const products = await result;
   res.status(200).json({ products, nbHits: products.length });
 };
 
