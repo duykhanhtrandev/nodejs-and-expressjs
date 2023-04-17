5356d7436d35d8f99c51dcb443d1334ec455daf5
Duy Khánh <109634649+duykhanhtrandev@users.noreply.github.com>
2023-04-17 16:33:56 +0700

    select option

--- 04-store-api/controllers/productsController.js
+++ 04-store-api/controllers/productsController.js
@@ -1,16 +1,16 @@
-const Product = require('../models/productModel');
+const Product = require("../models/productModel");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({}).sort('-name price');
+  const products = await Product.find({}).select("name price");
   res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
   const queryObject = {};
 
   if (featured) {
-    queryObject.featured = featured === 'true' ? true : false;
+    queryObject.featured = featured === "true" ? true : false;
   }
 
   if (company) {
@@ -18,17 +18,24 @@ const getAllProducts = async (req, res) => {
   }
 
   if (name) {
-    queryObject.name = { $regex: name, $options: 'i' };
+    queryObject.name = { $regex: name, $options: "i" };
   }
 
   // console.log(queryObject);
   let result = Product.find(queryObject);
+  // sort
   if (sort) {
-    const sortList = sort.split(',').join(' ');
+    const sortList = sort.split(",").join(" ");
     result = result.sort(sortList);
   } else {
-    result = result.sort('createdAt');
+    result = result.sort("createdAt");
   }
+
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ");
+    result = result.select(fieldsList);
+  }
+
   const products = await result;
   res.status(200).json({ products, nbHits: products.length });
 };
