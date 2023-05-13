57fbe726b0b5c2c042ff7d6ca0e098fd29afd86f
Duy Khánh <109634649+duykhanhtrandev@users.noreply.github.com>
2023-05-13 10:04:18 +0700

    create job route

--- 06-jobs-api/models/jobModel.js
+++ 06-jobs-api/models/jobModel.js
@@ -4,12 +4,12 @@ const JobSchema = new mongoose.Schema(
   {
     company: {
       type: String,
-      require: [true, "Please provide company name"],
+      required: [true, "Please provide company name"],
       maxlength: 50,
     },
     position: {
       type: String,
-      require: [true, "Please provide position"],
+      required: [true, "Please provide position"],
       maxlength: 100,
     },
     status: {
@@ -20,7 +20,7 @@ const JobSchema = new mongoose.Schema(
     createdBy: {
       type: mongoose.Types.ObjectId,
       ref: "User",
-      require: [true, "Please provide user"],
+      required: [true, "Please provide user"],
     },
   },
   { timestamps: true }
