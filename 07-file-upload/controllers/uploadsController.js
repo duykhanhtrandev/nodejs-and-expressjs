9b2dfa00b581461b2521715e652028d5a08619c4
Duy Khánh <109634649+duykhanhtrandev@users.noreply.github.com>
2023-05-20 10:48:44 +0700

    additional checks

--- 07-file-upload/controllers/uploadsController.js
+++ 07-file-upload/controllers/uploadsController.js
@@ -1,8 +1,27 @@
 const path = require("path");
 const { StatusCodes } = require("http-status-codes");
+const { BadRequestError } = require("../errors");
 
 const uploadProductImage = async (req, res) => {
+  // check if file exists
+  // check format
+  // check size
+
+  if (!req.files) {
+    throw new BadRequestError("No File Uploaded!");
+  }
   const productImage = req.files.image;
+
+  if (!productImage.mimetype.startsWith("image")) {
+    throw new BadRequestError("Please Upload Image!");
+  }
+
+  const maxSize = 50 * 1024 * 1024;
+
+  if (productImage.size > maxSize) {
+    throw new BadRequestError("Please upload image smaller than 5KB");
+  }
+
   const imagePath = path.join(
     __dirname,
     "../public/uploads/" + `${productImage.name}`
