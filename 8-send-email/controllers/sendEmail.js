6b02a1cfaf7542fe13f92557ac24c5ba6bb0e1d2
Duy Khánh <109634649+duykhanhtrandev@users.noreply.github.com>
2023-05-24 10:19:40 +0700

    send emails with sendgrid

--- 8-send-email/controllers/sendEmail.js
+++ 8-send-email/controllers/sendEmail.js
@@ -1,6 +1,7 @@
 const nodemailer = require("nodemailer");
+const sgMail = require("@sendgrid/mail");
 
-const sendEmail = async (req, res) => {
+const sendEmailEthereal = async (req, res) => {
   let testAccount = await nodemailer.createTestAccount();
 
   const transporter = nodemailer.createTransport({
@@ -19,6 +20,19 @@ const sendEmail = async (req, res) => {
     html: "<h2>Sending Emails with Node.js</h2>",
   });
 
+  res.json(info);
+};
+
+const sendEmail = async (req, res) => {
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  const msg = {
+    to: process.env.TO_EMAIL,
+    from: process.env.FROM_EMAIL,
+    subject: "Sending with SendGrid is Fun",
+    text: "and easy to do anywhere, even with Node.js",
+    html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+  };
+  const info = await sgMail.send(msg);
   res.json(info);
 };
 
